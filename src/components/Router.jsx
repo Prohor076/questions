15f98f7f24596b420afffd7fbf6505cd7ae33d93
Prohor072 <kirill-01-11@mail.ru>
2024-08-23 00:56:32 +0300

    fix fix

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -6,8 +6,8 @@ import App from "./App";
 const Router = () => {
     return <BrowserRouter>
         <Routes>
-            <Route element={<App/>} path='/questions'></Route>
-            <Route element={<Quiz/>} path='/questions/quiz/:id'></Route>
+            <Route element={<App/>} path='/'></Route>
+            <Route element={<Quiz/>} path='/quiz/:id'></Route>
 
             <Route element={<div>404 Not Found</div>} path='*'></Route>
         </Routes>
